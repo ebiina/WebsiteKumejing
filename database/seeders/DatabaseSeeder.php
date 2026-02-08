b53e4344e53a2c4d0545c5a8db4f2dcff5ab5a5f
ebiina <essay.mukti@mhs.unsoed.ac.id>
2026-02-09 06:35:10 +0700

    fix: call specialized seeders in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,20 +26,11 @@ class DatabaseSeeder extends Seeder
             ]
         );
 
-        // Initial Village Profile
-        \App\Models\VillageProfile::updateOrCreate(
-            ['id' => 1],
-            [
-                'history' => 'Sejarah Desa Kumejing...',
-                'vision' => 'Visi Desa Kumejing...',
-                'mission' => 'Misi Desa Kumejing...',
-                'location_map' => 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15816.634621577969!2d109.9176342!3d-7.666063!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e654eb68df6d9d1%3A0xe67c858e3f429b6!2sDesa%20Kumejing!5e0!3m2!1sen!2sid!4v1700000000000',
-            ]
-        );
-
-        // Initial Population Stats
-        \App\Models\PopulationStat::create(['label' => 'Total Penduduk', 'count' => 1500, 'category' => 'general']);
-        \App\Models\PopulationStat::create(['label' => 'Laki-laki', 'count' => 740, 'category' => 'gender']);
-        \App\Models\PopulationStat::create(['label' => 'Perempuan', 'count' => 760, 'category' => 'gender']);
+        // Run specialized seeders
+        $this->call([
+            VillageProfileSeeder::class,
+            VillageOfficialSeeder::class,
+            PopulationStatSeeder::class,
+        ]);
     }
 }
